5086486be5e533505ac5dc98b846c15641cef0ce
qzark1 <qzark1@yandex.ru>
2017-09-16 18:50:12 +0300

    Add files via upload

---
+++ src/main/java/com/example3/demo/Greeting.java
@@ -0,0 +1,28 @@
+package com.example3.demo;
+
+/**
+ * Created by root on 15.09.17.
+ */
+
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
